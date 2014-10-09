48412f48384fbd23da39092b87c9f323177c1f73
ababup1192 <ababup1192@gmail.com>
2014-10-09 23:13:40 +0900

    add draggable marker

--- coffee/map.coffee
+++ coffee/map.coffee
@@ -1,3 +1,31 @@
 $ ->
+	latlng = {
+		lat: 35.681382
+		lng: 139.766084
+	}
+
 	L.mapbox.accessToken = 'pk.eyJ1IjoiYWJhYnVwMTE5MiIsImEiOiJhb2JBNW5BIn0.IID695V8Pc8STRTeGaiMbg'
-	map = L.mapbox.map('map', 'examples.map-i86nkdio').setView([35.681382, 139.766084], 16)
+	map = L.mapbox.map('map', 'examples.map-i86nkdio').setView([latlng.lat, latlng.lng], 16)
+
+	marker = new Marker(latlng, 'えびの場所', map)
+	marker.addMap()
+
+class Marker
+	constructor: (@latlng, @message, @map) ->
+		@marker = L.marker(new L.LatLng(latlng.lat, latlng.lng), {
+			 	icon: L.mapbox.marker.icon({
+			 			'marker-color': 'ff8856'
+			 		}),
+			 	draggable: true
+			 })
+		@marker.bindPopup ' Laglng(' + latlng.lat + ', ' + latlng.lng + ')'
+		@marker.on 'dragend', (e) ->
+			latlng =  e.target._latlng
+			e.target.bindPopup String(latlng)
+
+	addMap: ->
+		@marker.addTo(@map)
+
+
+
+
--- dist/js/map.js
+++ dist/js/map.js
@@ -1,5 +1,39 @@
+var Marker;
+
 $(function() {
-  var map;
+  var latlng, map, marker;
+  latlng = {
+    lat: 35.681382,
+    lng: 139.766084
+  };
   L.mapbox.accessToken = 'pk.eyJ1IjoiYWJhYnVwMTE5MiIsImEiOiJhb2JBNW5BIn0.IID695V8Pc8STRTeGaiMbg';
-  return map = L.mapbox.map('map', 'examples.map-i86nkdio').setView([35.681382, 139.766084], 16);
+  map = L.mapbox.map('map', 'examples.map-i86nkdio').setView([latlng.lat, latlng.lng], 16);
+  marker = new Marker(latlng, 'えびの場所', map);
+  return marker.addMap();
 });
+
+Marker = (function() {
+  function Marker(latlng, message, map) {
+    this.latlng = latlng;
+    this.message = message;
+    this.map = map;
+    this.marker = L.marker(new L.LatLng(latlng.lat, latlng.lng), {
+      icon: L.mapbox.marker.icon({
+        'marker-color': 'ff8856'
+      }),
+      draggable: true
+    });
+    this.marker.bindPopup(' Laglng(' + latlng.lat + ', ' + latlng.lng + ')');
+    this.marker.on('dragend', function(e) {
+      latlng = e.target._latlng;
+      return e.target.bindPopup(String(latlng));
+    });
+  }
+
+  Marker.prototype.addMap = function() {
+    return this.marker.addTo(this.map);
+  };
+
+  return Marker;
+
+})();
